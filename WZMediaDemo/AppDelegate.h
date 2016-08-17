6a56bfdff5714637ce1598d38539cc9deca69dcc
zhangwx <worthyzhang@gmail.com>
2016-08-17 15:50:43 +0800

    Initial Commit

---
+++ WZMediaDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WZMediaDemo
+//
+//  Created by Worthy on 16/8/17.
+//  Copyright © 2016年 Worthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WZMediaDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WZMediaDemo
+//
+//  Created by Worthy on 16/8/17.
+//  Copyright © 2016年 Worthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
